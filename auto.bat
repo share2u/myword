0c55e8072bcef7c16fb1dfa5e195705f39ceeeb7
share2u <1076998973@qq.com>
2017-08-02 15:20:32 +0800

    'leetcode-louyuting'

---
+++ auto.bat
@@ -0,0 +1,15 @@
+echo "start git add commit fetch merge push"
+echo "git add -A"
+git add -A
+
+echo "git commit -m 'leetcode-louyuting'"
+git commit -m 'leetcode-louyuting'
+
+echo "git fetch origin master"
+git fetch origin master
+
+echo "git merge origin/master"
+git merge origin/master
+
+echo "git push origin master:master"
+git push origin master:master
